a6cbb915ddd21371bf5f3a37d39ec478229f70ad
ahsanparvezz <ahsan.p1012@gmail.com>
2025-12-20 20:19:15 +0700

    menambahkan logout

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 from main.models import UserProfile
 
@@ -160,3 +163,19 @@ def register(request):
         "role_type": role_type,
         "message": "User created successfully!"
     }, status=200)
+
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
+        }, status=401)
